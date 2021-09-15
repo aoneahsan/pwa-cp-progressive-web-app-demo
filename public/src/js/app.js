9d5b8217fb75f40d858f898590519e4220506b00
Ahsan Mahmood <ahsan@perkforce.com>
2021-09-15 18:08:36 +0500

    fix: updated

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -17,3 +17,32 @@ let pwaInstallPromptEvent
 //   event.preventDefault()
 //   return false
 // })
+
+// Check if Notifications available in window
+const notificationsAvailableInWindow = 'Notification' in window
+if (notificationsAvailableInWindow) {
+  const askForNotificationsPermissions = () => {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        console.log('Notification Permission granted.')
+      } else if (permission === 'denied') {
+        console.log('Notification Permission denied.')
+      } else {
+        console.log('Notification Permission cancelled.')
+      }
+    })
+  }
+
+  const enableNotificationsButtons = document.querySelectorAll(
+    '.enable-notifications'
+  )
+  for (let i = 0; i < enableNotificationsButtons.length; i++) {
+    const btn = enableNotificationsButtons[i]
+
+    // set display to inline-block
+    btn.style.display = 'inline-block'
+
+    // add click listener
+    btn.addEventListener('click', askForNotificationsPermissions)
+  }
+}
