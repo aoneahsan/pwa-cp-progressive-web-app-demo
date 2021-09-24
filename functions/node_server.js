b958188f6a4966fa7a07eb595399dbfc7bf64910
Ahsan Mahmood <ahsan@perkforce.com>
2021-09-24 14:16:16 +0500

    fix: updated

--- functions/node_server.js
+++ functions/node_server.js
@@ -34,21 +34,6 @@ const googleCloudStorageConfig = {
   keyFilename: 'pwa-firebase-keys.json'
 }
 
-const dataURItoBlob = dataURI => {
-  var byteString = atob(dataURI.split(',')[1])
-  var mimeString = dataURI
-    .split(',')[0]
-    .split(':')[1]
-    .split(';')[0]
-  var ab = new ArrayBuffer(byteString.length)
-  var ia = new Uint8Array(ab)
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i)
-  }
-  var blob = new Blob([ab], { type: mimeString })
-  return blob
-}
-
 app.post('/postdata', (req, res, next) => {
   cors(req, res, () => {
     const uuid = UUID_V4()
