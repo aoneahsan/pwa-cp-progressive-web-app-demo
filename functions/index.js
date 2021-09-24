821852ace5b27bd21009cb819c32754c1049b1f2
Ahsan Mahmood <ahsan@perkforce.com>
2021-09-24 13:58:01 +0500

    fix: updated

--- functions/index.js
+++ functions/index.js
@@ -35,98 +35,104 @@ exports.storePostData = functions.https.onRequest((request, response) => {
     const uuid = UUID_V4()
     const formData = formidable.IncomingForm()
     const gcStorage = new Storage(googleCloudStorageConfig)
-    formData.parse(request, (err, fields, files) => {
+    formData.parse(req, (err, fields, files) => {
+      console.log('formData', { err, fields, files })
       if (err) {
         console.error(
           'Error Occured while parsing formData, formidable, err:',
           err
         )
-        response.status(500).json({ error: err, message: err.message })
+        res.status(500).json({ error: err, message: err.message })
       } else {
-        fs.rename(files.image.path, '/tmp/' + files.image.name)
-        const bucket = gcStorage.bucket('pwa-cp.appspot.com')
+        const { id, title, location } = fields
+        const { image } = files
 
-        bucket.upload(
-          '/tmp/' + files.image.name,
-          {
-            uploadType: 'media',
-            metadata: {
-              metadata: {
-                contentType: files.image.type,
-                firebaseStorageDownloadTokens: uuid
-              }
-            }
-          },
-          (err, uploadedFile) => {
-            if (err) {
-              console.error(
-                'Error Occured while uploading file to bucket, err:',
-                err
-              )
-              response.status(500).json({ error: err, message: err.message })
-            } else {
-              const { id, title, location } = fields
-              const postData = {
-                id,
-                title,
-                location,
-                image: `https://firebasestorage.googleapis.com/v0/b/${
-                  bucket.name
-                }/o/${encodeURIComponent(
-                  uploadedFile.name
-                )}?alt=media&token=${uuid}`
-              }
-              admin
-                .database()
-                .ref(POSTS_TABLE)
-                .push(postData)
-                .then(res => {
-                  return admin
-                    .database()
-                    .ref(SUBSCRIPTIONS_TABLE)
-                    .once('value')
-                })
-                .then(subScriptions => {
-                  // define webpush vapid details
-                  webPush.setVapidDetails(
-                    'mailto:aoneahsan@gmail.com',
-                    vapidPublicKey,
-                    vapidPrivateKey
-                  )
-
-                  // send web push notification to add subscriptions
-                  subScriptions.forEach(sub => {
-                    const pushConfig = sub.val()
-                    const pushData = JSON.stringify({
-                      title: 'Post Created!',
-                      content: 'New post created successfully.',
-                      url: 'http://localhost:8080/help'
-                    })
-                    webPush
-                      .sendNotification(pushConfig, pushData)
-                      .catch(err => {
-                        console.error(
-                          '[Index.js] error while sending web push notification, err: ',
-                          err
-                        )
-                      })
-                  })
-
-                  // send response
-                  response.status(201).json({ message: 'Data Stored.', id: id })
-                })
-                .catch(err => {
-                  console.error(
-                    'Error Occured while storing data in database, err:',
-                    err
-                  )
-                  response
-                    .status(500)
-                    .json({ error: err, message: err.message })
-                })
-            }
+        fs.rename(image.path, '/tmp/' + image.name, err => {
+          if (err) {
+            console.error('Error Occured while renaming file, err:', err)
+            res.status(500).json({ error: err, message: err.message })
           }
-        )
+          const bucket = gcStorage.bucket('pwa-cp.appspot.com')
+
+          bucket.upload(
+            '/tmp/' + image.name,
+            {
+              uploadType: 'media',
+              metadata: {
+                metadata: {
+                  contentType: image.type,
+                  firebaseStorageDownloadTokens: uuid
+                }
+              }
+            },
+            (err, uploadedFile) => {
+              if (err) {
+                console.error(
+                  'Error Occured while uploading file to bucket, err:',
+                  err
+                )
+                res.status(500).json({ error: err, message: err.message })
+              } else {
+                const postData = {
+                  id,
+                  title,
+                  location,
+                  image: `https://firebasestorage.googleapis.com/v0/b/${
+                    bucket.name
+                  }/o/${encodeURIComponent(
+                    uploadedFile.name
+                  )}?alt=media&token=${uuid}`
+                }
+                admin
+                  .database()
+                  .ref(POSTS_TABLE)
+                  .push(postData)
+                  .then(res => {
+                    return admin
+                      .database()
+                      .ref(SUBSCRIPTIONS_TABLE)
+                      .once('value')
+                  })
+                  .then(subScriptions => {
+                    // define webpush vapid details
+                    webPush.setVapidDetails(
+                      'mailto:aoneahsan@gmail.com',
+                      vapidPublicKey,
+                      vapidPrivateKey
+                    )
+
+                    // send web push notification to add subscriptions
+                    subScriptions.forEach(sub => {
+                      const pushConfig = sub.val()
+                      const pushData = JSON.stringify({
+                        title: 'Post Created!',
+                        content: 'New post created successfully.',
+                        url: 'http://localhost:8080/help'
+                      })
+                      webPush
+                        .sendNotification(pushConfig, pushData)
+                        .catch(err => {
+                          console.error(
+                            '[Index.js] error while sending web push notification, err: ',
+                            err
+                          )
+                        })
+                    })
+
+                    // send res
+                    res.status(201).json({ message: 'Data Stored.', id: id })
+                  })
+                  .catch(err => {
+                    console.error(
+                      'Error Occured while storing data in database, err:',
+                      err
+                    )
+                    res.status(500).json({ error: err, message: err.message })
+                  })
+              }
+            }
+          )
+        })
       }
     })
   })
